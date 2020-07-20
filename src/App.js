02c7973d4758719f2d250ece507eac34eeb999a7
Jack Mallek <cloneguy77@gmail.com>
2020-07-20 17:06:53 -0400

    added hello message to front page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload you bitch
         </p>
         <a
           className="App-link"
